840981f65eab75d9f9c135f71e8d8254b480a21b
Alex Fedosin <nsdfxela@gmail.com>
2014-04-27 21:04:26 +0400

    first working version

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,8 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## These functions implement cached version of standart solve functions.
+## makeCacheMatrix creates "cachedMatrix" which is actually a vector
+## consists of accessors and datafiels. cacheSolve includes a logic of 
+## choosing the way of calculation inverse (normal, via standart "solve", 
+## or cached, if the value have already be calculated)
 
 makeCacheMatrix <- function(x = matrix()) {
 
@@ -17,9 +18,20 @@ makeCacheMatrix <- function(x = matrix()) {
   
 }
 
-
-## Write a short comment describing this function
+## cacheSolve implements cached version of "solve"
+## needs an object created by makeCacheMatrix function on input
 
 cacheSolve <- function(x, ...) {
+  
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getInv()
+  if(is.null(i)){ ##if no data in cash
+    data <- x$get()
+    i <- solve(data)
+    x$setInv(i) ##writing in cash
+    message("non-cached")
+    return(i)
+  }
+  message("cached")
+  i
 }
